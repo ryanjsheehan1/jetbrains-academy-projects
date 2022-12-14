739499deccc5bde43a48b45bd905009e2df76ca4
Ryan Sheehan <83296615+ryanjsheehan1@users.noreply.github.com>
2022-12-14 16:35:00 -0600

    Create Main.java

---
+++ simple-tic-tac-toe/Main.java
@@ -0,0 +1,10 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe newGame = new TicTacToe();
+        newGame.startGame();
+    }
+}
