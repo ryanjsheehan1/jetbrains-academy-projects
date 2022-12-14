7270cf7d16e15f100da7ea78007e767f5277f090
Ryan Sheehan <83296615+ryanjsheehan1@users.noreply.github.com>
2022-12-14 16:36:12 -0600

    Create Application.java

---
+++ coffee-machine/Application.java
@@ -0,0 +1,8 @@
+package machine;
+
+public class Application {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.startUI();
+    }
+}
